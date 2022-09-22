da806d311bda1fd953450bc32415a4afec7cd412
Yinjtcflm <1529792769@163.com>
2022-09-23 00:42:09 +0800

    deploy

---
+++ precache-manifest.ae42c2083b07af6f1d499318a4c29488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268353b02a64c389be62",
+    "url": "/account-website/css/app.230047eb.css"
+  },
+  {
+    "revision": "11cfd764447936b81a2ca9aea011f891",
+    "url": "/account-website/index.html"
+  },
+  {
+    "revision": "268353b02a64c389be62",
+    "url": "/account-website/js/app.03c54edd.js"
+  },
+  {
+    "revision": "05fcf0a3398f54ea3696",
+    "url": "/account-website/js/chunk-vendors.d447e715.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-website/robots.txt"
+  }
+]);
